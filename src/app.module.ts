e62c0bda8e338a3d761174c0ff5c77b305b512a1
mansur <sir.makhmutov@gmail.com>
2022-05-01 22:55:55 +0300

    config package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,22 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypegooseModule} from 'nestjs-typegoose'
 
 @Module({
-  imports: [AuthModule, ProductModule, ReviewModule, TopPageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory:getMongoConfig
+    }),
+    AuthModule,
+    ProductModule,
+    ReviewModule,
+    TopPageModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -3,9 +3,13 @@ import {ProductModel} from "../product/product.model";
 import {FindProductDto} from "../product/dto/find-product.dto";
 import {TopPageModel} from "./top-page.model";
 import {FindTopPageDto} from "./dto/find-top-page.dto";
+import {ConfigService} from "@nestjs/config";
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService:ConfigService) {
+    this.configService.get('TEST')
+  }
 
   @Post('create')
   async create(@Body() dto:Omit<TopPageModel, '_id'>){}
