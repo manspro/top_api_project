c4fc23e7677fbffa5e5ba33a951a24e6063e6e08
mansur <sir.makhmutov@gmail.com>
2022-05-08 18:11:45 +0300

    test e2e

---
+++ test/review.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { disconnect, Types } from 'mongoose';
+import { CreateReviewDto } from '../src/review/dto/create-review.dto';
+
+const productId = new Types.ObjectId().toHexString();
+
+const testDto: CreateReviewDto = {
+  name: 'Test',
+  title: 'Header',
+  description: 'Description test',
+  rating: 5,
+  productId,
+};
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let createdId: string;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/review/create (POST)', async done => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send(testDto)
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        createdId = body._id;
+        expect(createdId).toBeDefined();
+        done();
+      });
+  });
+
+  afterAll(() => {
+    disconnect();
+  });
+});
