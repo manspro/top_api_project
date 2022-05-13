e15714bc11a61bb4cf821369618d57c85772bf0a
mansur <sir.makhmutov@gmail.com>
2022-05-13 22:48:51 +0300

    create validateuser method

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,2 @@
 export const ALREADY_REGISTERED_ERROR = 'Такой пользователь уже существует';
+export const USER_NOR_FOUND_ERROR = 'Пользователь с таким email не найден';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { InjectModel } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { genSaltSync, hashSync } from 'bcryptjs';
+import { USER_NOR_FOUND_ERROR } from './auth.constants';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
   ) {}
+
   async createUser(dto: AuthDto) {
     const salt = genSaltSync(10);
     const newUser = new this.userModel({
@@ -21,5 +23,12 @@ export class AuthService {
 
   async findUser(email: string) {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.findUser(email);
+    if (!user) {
+      throw new UnauthorizedException(USER_NOR_FOUND_ERROR);
+    }
   }
 }
