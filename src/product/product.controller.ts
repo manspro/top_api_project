6ce7a9dc77370bcda24d282bb1c74792f4b6d8aa
mansur <sir.makhmutov@gmail.com>
2022-05-18 01:50:08 +0300

    create product service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,33 +1,37 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { FindProductDto } from './dto/find-product.dto';
 import { ProductModel } from './product.model';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
+  constructor(private readonly productService: ProductService) {}
 
-	@Post('create')
-	async create(@Body() dto: Omit<ProductModel, '_id'>) {
+  @Post('create')
+  async create(@Body() dto: CreateProductDto) {
+    return this.productService.create(dto);
+  }
 
-	}
+  @Get(':id')
+  async get(@Param('id') id: string) {}
 
-	@Get(':id')
-	async get(@Param('id') id: string) {
+  @Delete(':id')
+  async delete(@Param('id') id: string) {}
 
-	}
+  @Patch(':id')
+  async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
 
-	@Delete(':id')
-	async delete(@Param('id') id: string) {
-
-	}
-
-	@Patch(':id')
-	async patch(@Param('id') id: string, @Body() dto: ProductModel) {
-
-	}
-
-	@HttpCode(200)
-	@Post()
-	async find(@Body() dto: FindProductDto) {
-
-	}
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: FindProductDto) {}
 }
