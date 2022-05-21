9372a7941c58b4b15ab1baaab392e4a0b88fc375
mansur <sir.makhmutov@gmail.com>
2022-05-21 16:56:18 +0300

    create top page dto

---
+++ src/top-page/dto/create-top-page.dto.ts
@@ -0,0 +1,71 @@
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { TopLevelCategory } from '../top-page.model';
+import {
+  IsAlpha,
+  IsArray,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class HhDataDto {
+  @IsNumber()
+  count: number;
+
+  @IsNumber()
+  juniorSalary: number;
+
+  @IsNumber()
+  middleSalary: number;
+
+  @IsNumber()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantageDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  description: string;
+}
+
+export class CreateTopPage {
+  @IsEnum(TopLevelCategory)
+  firstCategory: TopLevelCategory;
+
+  @IsString()
+  secondCategory: string;
+
+  @IsString()
+  alias: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => HhDataDto)
+  hh?: HhDataDto;
+
+  @IsArray()
+  @Type(() => TopPageAdvantageDto)
+  advantages: TopPageAdvantageDto[];
+
+  @IsString()
+  seoText: string;
+
+  @IsString()
+  tagsTitle: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  tags: string[];
+}
